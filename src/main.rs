2f42a57ae82036be3b2e341064f3f4eb31120179
Daniel Williams <dwilli@kth.se>
2023-01-25 22:21:18 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("AES Implementation coming up...");
+}
+
+fn substitute_bytes() {
+    !unimplemented!()
+}
+
+fn shift_rows(){
+    !unimplemented!()
+}
+
+fn mix_columns(){
+    !unimplemented!()
+}
