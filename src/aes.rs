a21b06a176aff87549ef63772474a65f699a24be
Daniel Williams <dwilli@kth.se>
2023-02-04 18:58:11 +0100

    Laptop -> Desktop

---
+++ src/aes.rs
@@ -0,0 +1,13 @@
+pub fn substitute_bytes() {
+    unimplemented!()
+}
+
+pub fn shift_rows(){
+    unimplemented!()
+}
+
+pub fn mix_columns(){
+    unimplemented!()
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
+mod aes;
+
 fn main() {
     println!("AES Implementation coming up...");
-}
 
-fn substitute_bytes() {
-    !unimplemented!()
-}
+    // Run the key expansion algorithm and save the round keys
 
-fn shift_rows(){
-    !unimplemented!()
-}
-
-fn mix_columns(){
-    !unimplemented!()
+    // Break up the incoming bytes into chunks of 16 bytes (128 bits).
+    // For each chunk, run the AES algorithm
+    aes::substitute_bytes();
 }
